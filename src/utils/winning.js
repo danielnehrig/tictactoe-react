d71ef5eb29ad8eee90c412b28797dbf34e3da5af
Daniel Nehrig <daniel.nehrig@redteclab.com>
2019-11-21 12:51:21 +0100

    win val logic passed

--- src/utils/winning.js
+++ src/utils/winning.js
@@ -4,6 +4,7 @@ export const winning = board => {
   let winner = 0
   let counter = 0
   let init = 0
+  let wincase = ''
 
   // Left to Right Horizontal Check [0][0] [0][1] [0][2]
   for (let i = 0; i < board.length; i++) {
@@ -22,7 +23,7 @@ export const winning = board => {
 
           if (counter === size) {
             winner = value
-            console.log('winner left 2 right')
+            wincase = 'left 2 right'
           }
         }
       } else {
@@ -30,20 +31,103 @@ export const winning = board => {
       }
     }
 
-    if (winner !== 0) break
+    if (winner !== 0) {
+      break
+    }
   }
 
   // Diagonal check [0][0] [1][1] [2][2]
+  if (winner === 0) {
+    init = 0
+    counter = 0
+    for (let i = 0; i < board.length; i++) {
+      const value = board[i][i]
 
-  // Diagonal check reverse [0][2] [1][1] [2][0]
+      if (value !== 0) {
+        if (init === 0) {
+          init = value
+        }
+
+        if (value === init) {
+          counter++
+
+          if (counter === size) {
+            winner = value
+            wincase = 'diag check top left bot right'
+          }
+        }
+      } else {
+        break
+      }
+    }
+  }
+
+  // Diagonal check reverse [2][0] [1][1] [0][2]
+  if (winner === 0) {
+    init = 0
+    counter = 0
+    for (let i = 0, k = 2; i < board.length; i++, k--) {
+      const value = board[k][i]
+
+      if (value !== 0) {
+        if (init === 0) {
+          init = value
+        }
+
+        if (value === init) {
+          counter++
+
+          if (counter === size) {
+            winner = value
+            wincase = 'diag check bot right top left'
+          }
+        }
+      } else {
+        break
+      }
+    }
+  }
 
   // Top Down Check [0][0] [1][0] [2][0]
   // --- ---- ----- [0][1] [1][1] [2][1]
   // --- ---- ----- [0][2] [1][2] [2][2]
+  if (winner === 0) {
+    for (let i = 0; i < board.length; i++) {
+      counter = 0
+      init = 0
+      for (let k = 0; k < board.length; k++) {
+        const value = board[k][i]
+        console.log(`[${k}][${i}] = ${value}`)
+
+        if (value !== 0) {
+          if (init === 0) {
+            init = value
+          }
+
+          if (value === init) {
+            counter++
+
+            if (counter === size) {
+              winner = value
+              wincase = 'top down'
+              break
+            }
+          }
+        } else {
+          break
+        }
+      }
+
+      if (winner !== 0) {
+        break
+      }
+    }
+  }
 
   const result = {
     isWinner: winner !== 0,
-    player: winner
+    player: winner,
+    wincase: wincase
   }
 
   return result
--- src/utils/winning.test.js
+++ src/utils/winning.test.js
@@ -14,19 +14,31 @@ import {
   failCase3
 } from 'src/mocks'
 
+const DIAG1 = 'diag check top left bot right'
+const DIAG2 = 'diag check bot right top left'
+const HOR = 'left 2 right'
+const TOPDOWN = 'top down'
+const FAILCASE = ''
+
 describe('diagonal tests', () => {
   test('test diag 1', () => {
     const win = winning(diagWin)
+    console.log(win.wincase)
+    expect(win.wincase).toBe(DIAG1)
     expect(win.isWinner).toBe(true)
   })
 
   test('test diag 2', () => {
     const win = winning(diagWin2)
+    console.log(win.wincase)
+    expect(win.wincase).toBe(DIAG2)
     expect(win.isWinner).toBe(true)
   })
 
   test('test diag 3', () => {
     const win = winning(diagWin3)
+    console.log(win.wincase)
+    expect(win.wincase).toBe(DIAG2)
     expect(win.isWinner).toBe(true)
   })
 })
@@ -34,16 +46,22 @@ describe('diagonal tests', () => {
 describe('horizontal tests', () => {
   test('test hor 1', () => {
     const win = winning(horWinLR)
+    console.log(win.wincase)
+    expect(win.wincase).toBe(HOR)
     expect(win.isWinner).toBe(true)
   })
 
   test('test hor 2', () => {
     const win = winning(horWinLR2)
+    console.log(win.wincase)
+    expect(win.wincase).toBe(HOR)
     expect(win.isWinner).toBe(true)
   })
 
   test('test hor 3', () => {
     const win = winning(horWinLR2)
+    console.log(win.wincase)
+    expect(win.wincase).toBe(HOR)
     expect(win.isWinner).toBe(true)
   })
 })
@@ -51,16 +69,22 @@ describe('horizontal tests', () => {
 describe('top down tests', () => {
   test('test top down 1', () => {
     const win = winning(topDown)
+    console.log(win.wincase)
+    expect(win.wincase).toBe(TOPDOWN)
     expect(win.isWinner).toBe(true)
   })
 
   test('test top down 2', () => {
     const win = winning(topDown2)
+    console.log(win.wincase)
+    expect(win.wincase).toBe(TOPDOWN)
     expect(win.isWinner).toBe(true)
   })
 
   test('test top down 3', () => {
     const win = winning(topDown3)
+    console.log(win.wincase)
+    expect(win.wincase).toBe(TOPDOWN)
     expect(win.isWinner).toBe(true)
   })
 })
@@ -68,16 +92,22 @@ describe('top down tests', () => {
 describe('fail case tests', () => {
   test('fail 1', () => {
     const win = winning(failCase)
+    console.log(win.wincase)
+    expect(win.wincase).toBe(FAILCASE)
     expect(win.isWinner).toBe(false)
   })
 
   test('fail 2', () => {
     const win = winning(failCase2)
+    console.log(win.wincase)
+    expect(win.wincase).toBe(FAILCASE)
     expect(win.isWinner).toBe(false)
   })
 
   test('fail 3', () => {
     const win = winning(failCase3)
+    console.log(win.wincase)
+    expect(win.wincase).toBe(FAILCASE)
     expect(win.isWinner).toBe(false)
   })
 })
